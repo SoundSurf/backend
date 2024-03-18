02e1b2bd4183ec6f3d8240487aade9fc50ec28f6
roganOhDev <fdscbjdcnhd@gmail.com>
2024-03-18 13:45:19 +0900

    exception handler

---
+++ src/main/java/com/api/soundsurf/api/exception/ExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.api.soundsurf.api.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionDto> exception(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(new ExceptionDto(e));
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ExceptionDto> exception(ConstraintViolationException e) {
+        return ResponseEntity.badRequest().body(new ExceptionDto(e));
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(ApiException.class)
+    public ResponseEntity<ExceptionDto> exception(ApiException e) {
+        return ResponseEntity.badRequest().body(new ExceptionDto(e));
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<ExceptionDto> exception(WebExchangeBindException e) {
+        return ResponseEntity.badRequest().body(new ExceptionDto(e));
+    }
+}
