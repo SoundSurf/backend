ea40ac7e2d95062ec49163eaebc3692c524f4a85
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-04-04 22:26:33 +0900

    feat: QR 조회 dto 구현

--- src/main/java/com/api/soundsurf/iam/dto/QrDto.java
+++ src/main/java/com/api/soundsurf/iam/dto/QrDto.java
@@ -1,7 +1,9 @@
 package com.api.soundsurf.iam.dto;
 
+import com.api.soundsurf.iam.entity.Qr;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 public class QrDto {
@@ -11,6 +13,27 @@ public class QrDto {
         @AllArgsConstructor
         public static class Response {
             private byte[] qrCode;
+        }
+    }
+
+    public static class find {
+        @Getter
+        @Schema(name = "QrDto.find.Request")
+        @AllArgsConstructor
+        @Builder
+        public static class Response {
+            private Long id;
+            private byte[] qrCode;
+            private Long userId;
+
+            public static Response of(Qr qr) {
+                return Response.builder()
+                        .id(qr.getId())
+                        .qrCode(qr.getQr())
+                        .userId(qr.getUser().getId())
+                        .build();
+            }
+
         }
     }
 }
