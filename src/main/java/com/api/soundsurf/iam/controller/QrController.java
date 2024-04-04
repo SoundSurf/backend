b90a9064b2c191d13406a9494d28258a402c4c00
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-04-04 22:27:13 +0900

    feat: QR 조회 API 구현

--- src/main/java/com/api/soundsurf/iam/controller/QrController.java
+++ src/main/java/com/api/soundsurf/iam/controller/QrController.java
@@ -6,6 +6,7 @@ import com.api.soundsurf.iam.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,11 @@ public class QrController {
     @PostMapping(value = "/create")
     public QrDto.Create.Response create(@AuthenticationPrincipal SessionUser sessionUser) {
         return transferService.create(sessionUser);
+    }
+
+    @GetMapping
+    public QrDto.find.Response find(@AuthenticationPrincipal SessionUser sessionUser) {
+        return transferService.find(sessionUser.getUserId());
     }
 
 }
