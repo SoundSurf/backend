d6bb95a7c6ff2802c6764feb077b7941934aff1e
roganOhDev <fdscbjdcnhd@gmail.com>
2024-04-15 18:50:10 +0900

    DI

--- src/main/java/com/api/soundsurf/api/quartz/QuartzJobGetSpotifyAccessToken.java
+++ src/main/java/com/api/soundsurf/api/quartz/QuartzJobGetSpotifyAccessToken.java
@@ -2,6 +2,7 @@ package com.api.soundsurf.api.quartz;
 
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static org.quartz.JobBuilder.newJob;
@@ -10,7 +11,16 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 @Component
 public class QuartzJobGetSpotifyAccessToken {
+    private final SpotifyJobFactory jobFactory;
+
+    @Autowired
+    public QuartzJobGetSpotifyAccessToken(SpotifyJobFactory jobFactory) {
+        this.jobFactory = jobFactory;
+    }
+
     public void conf(final Scheduler scheduler) throws SchedulerException {
+        scheduler.setJobFactory(jobFactory);
+
         final var job = newJob(GetSpotifyAccessTokenJob.class)
                 .withIdentity("spotifyToken", "group1")
                 .build();
---
+++ src/main/java/com/api/soundsurf/api/quartz/SpotifyJobFactory.java
@@ -0,0 +1,29 @@
+package com.api.soundsurf.api.quartz;
+
+import com.api.soundsurf.iam.domain.SpotifyTokenProvider;
+import org.quartz.Job;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.spi.JobFactory;
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpotifyJobFactory implements JobFactory {
+
+    private final SpotifyTokenProvider spotifyTokenProvider;
+
+    @Autowired
+    public SpotifyJobFactory(SpotifyTokenProvider spotifyTokenProvider) {
+        this.spotifyTokenProvider = spotifyTokenProvider;
+    }
+
+    @Override
+    public Job newJob(TriggerFiredBundle bundle, Scheduler scheduler) throws SchedulerException {
+        if (bundle.getJobDetail().getJobClass().equals(GetSpotifyAccessTokenJob.class)) {
+            return new GetSpotifyAccessTokenJob(spotifyTokenProvider);
+        }
+        throw new SchedulerException("Invalid job class");
+    }
+}
