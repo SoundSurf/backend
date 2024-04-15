6fd6a1281786230ea49c949229e7a1975193cc5c
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-04-15 20:17:10 +0900

    feat: recommendation 음악 임시 구현

--- src/main/java/com/api/soundsurf/iam/controller/SpotifyController.java
+++ src/main/java/com/api/soundsurf/iam/controller/SpotifyController.java
@@ -2,10 +2,16 @@ package com.api.soundsurf.iam.controller;
 
 import com.api.soundsurf.iam.domain.SpotifyService;
 import com.api.soundsurf.iam.dto.MusicDto;
+import com.api.soundsurf.iam.dto.SessionUser;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.hc.core5.http.ParseException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +28,39 @@ public class SpotifyController {
 
 
     @GetMapping("/search")
-    public List<MusicDto.Search.Response> search(@Valid @RequestBody MusicDto.Search.Request request) throws IOException, ParseException, SpotifyWebApiException {
+    @Operation(
+            parameters = {
+                    @Parameter(name = "authorization", in = ParameterIn.HEADER,
+                            required = true, content = @Content(mediaType = "application/json"))
+            })
+    public List<MusicDto.Common.Response> search(
+            final @AuthenticationPrincipal SessionUser sessionUser,
+            @Valid @RequestBody MusicDto.Search.Request request) throws IOException, ParseException, SpotifyWebApiException {
         return service.search(request);
     }
 
     @GetMapping("/genres")
-    public MusicDto.Genre.Response getGenres() throws IOException, ParseException, SpotifyWebApiException {
+    @Operation(
+            parameters = {
+                    @Parameter(name = "authorization", in = ParameterIn.HEADER,
+                            required = true, content = @Content(mediaType = "application/json"))
+            })
+    public MusicDto.Genre.Response getGenres(
+            final @AuthenticationPrincipal SessionUser sessionUser
+    ) throws IOException, ParseException, SpotifyWebApiException {
         return service.getGenres();
+    }
+
+    @GetMapping("/recommendation")
+    @Operation(
+            parameters = {
+                    @Parameter(name = "authorization", in = ParameterIn.HEADER,
+                            required = true, content = @Content(mediaType = "application/json"))
+            })
+    public List<MusicDto.Common.Response> recommendation(
+            final @AuthenticationPrincipal SessionUser sessionUser,
+            @Valid @RequestBody MusicDto.Recommendation.Request request) throws IOException, ParseException, SpotifyWebApiException {
+        return service.recommendation(request);
     }
 
 
--- src/main/java/com/api/soundsurf/iam/dto/MusicDto.java
+++ src/main/java/com/api/soundsurf/iam/dto/MusicDto.java
@@ -1,5 +1,6 @@
 package com.api.soundsurf.iam.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,15 +8,10 @@ import java.util.List;
 
 public class MusicDto {
 
-    public static class Search {
-
-        @Getter
-        public static class Request {
-            private String title;
-        }
-
+    public static class Common {
         @Getter
         @AllArgsConstructor
+        @Schema(name = "MusicDto.Common.Response")
         public static class Response {
             private String albumName;
             private List<String> artistName;
@@ -27,11 +23,31 @@ public class MusicDto {
         }
     }
 
+    public static class Search {
+        @Getter
+        @Schema(name = "MusicDto.Search.Request")
+        public static class Request {
+            private String title;
+
+        }
+
+
+    }
+
     public static class Genre {
         @Getter
         @AllArgsConstructor
+        @Schema(name = "MusicDto.Genre.Response")
         public static class Response {
             private List<String> genres;
+        }
+    }
+
+    public static class Recommendation {
+        @Getter
+        @Schema(name = "MusicDto.Recommendation.Request")
+        public static class Request {
+            private String genre;
         }
     }
 }
