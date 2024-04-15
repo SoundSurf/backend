524171fd8333aa72de784f68cd4b919b8efe435b
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-04-15 18:15:30 +0900

    feat: spotify access token provider 구현

---
+++ src/main/java/com/api/soundsurf/iam/domain/SpotifyTokenProvider.java
@@ -0,0 +1,26 @@
+package com.api.soundsurf.iam.domain;
+
+import com.wrapper.spotify.SpotifyApi;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SpotifyTokenProvider {
+
+    private final SpotifyApi spotifyApi;
+
+    public void getAccessToken() {
+        final var clientCredentialsRequest = spotifyApi.clientCredentials().build();
+
+        try {
+            final var clientCredentials = clientCredentialsRequest.execute();
+            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+            log.info("Access token: " + clientCredentials.getAccessToken());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
