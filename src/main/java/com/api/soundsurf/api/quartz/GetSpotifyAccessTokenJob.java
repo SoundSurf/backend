ac8c3fd640fa8778d558220fdcc375caf27ed452
roganOhDev <fdscbjdcnhd@gmail.com>
2024-04-15 16:40:54 +0900

    fix to log

--- src/main/java/com/api/soundsurf/api/quartz/GetSpotifyAccessTokenJob.java
+++ src/main/java/com/api/soundsurf/api/quartz/GetSpotifyAccessTokenJob.java
@@ -1,5 +1,6 @@
 package com.api.soundsurf.api.quartz;
 
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -8,10 +9,11 @@ import java.time.LocalDateTime;
 
 import static com.api.soundsurf.api.Const.SPOTIFY_ACCESS_TOKEN;
 
+@Slf4j
 public class GetSpotifyAccessTokenJob implements Job {
     public void execute(JobExecutionContext arg0) throws JobExecutionException {
         System.setProperty(SPOTIFY_ACCESS_TOKEN, "abc");
-        System.out.println("This is a quartz job example, please fix me!" + LocalDateTime.now());
+        log.info("This is a quartz job example, please fix me!" + LocalDateTime.now());
 //        System.out.println(System.getProperty(SPOTIFY_ACCESS_TOKEN));
     }
 }
