c2e50d28e5b0af79e1b3065b101a2c25f93071dc
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-05-09 14:38:26 +0900

    feat: custom exception 추가

---
+++ src/main/java/com/api/soundsurf/iam/exception/SpotifyNowPlayingException.java
@@ -0,0 +1,12 @@
+package com.api.soundsurf.iam.exception;
+
+import com.api.soundsurf.api.exception.ApiException;
+import com.api.soundsurf.api.exception.ExceptionCode;
+
+public class SpotifyNowPlayingException extends ApiException {
+    private final static String exceptionCode = ExceptionCode.SPOTIFY.SPOTIFY_ALBUM_INFO_EXCEPTION;
+
+    public SpotifyNowPlayingException(String message) {
+        super(exceptionCode + " : " + message);
+    }
+}
