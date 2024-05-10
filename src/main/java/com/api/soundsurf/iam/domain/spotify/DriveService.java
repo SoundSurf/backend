b3b44a3548486f301bc9822a8fab4c30500fd92b
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-05-11 02:08:58 +0900

    feat: 앨범 조회시 장르와 평점 크롤링 하여 표시

--- src/main/java/com/api/soundsurf/iam/domain/spotify/DriveService.java
+++ src/main/java/com/api/soundsurf/iam/domain/spotify/DriveService.java
@@ -11,10 +11,8 @@ import org.apache.hc.core5.http.ParseException;
 import org.springframework.stereotype.Service;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
-import se.michaelthelin.spotify.model_objects.specification.Album;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.api.soundsurf.iam.domain.spotify.Utils.convertToTrackDtoList;
@@ -33,8 +31,8 @@ public class DriveService {
                 genres = GenreType.getRandomGenres(5);
             }
 
-            List<String> genreStrings = genres.stream().map(GenreType::getValue).collect(Collectors.toList());
-            final String joinedGenres = String.join(",", genreStrings);
+            final var genreStrings = genres.stream().map(GenreType::getValue).collect(Collectors.toList());
+            final var joinedGenres = String.join(",", genreStrings);
 
             final var recommendations = api.getRecommendations()
                     .seed_genres(joinedGenres)
@@ -50,11 +48,13 @@ public class DriveService {
         }
     }
 
-    public MusicDto.NowPlaying.Response getNowPlaying(final String albumId) {
-        // TODO : 크롤러 사용하여 RYM에서 장르, 평점 가져오기
+    public MusicDto.NowPlaying.Response getNowPlayingAlbum(final String albumId) {
         try {
-            final Album album = api.getAlbum(albumId).build().execute();
-            return new MusicDto.NowPlaying.Response(new MusicDto.AlbumFullInfo.Info(album));
+            final var album = api.getAlbum(albumId).build().execute();
+            final var artist = album.getArtists()[0].getName().toLowerCase().replace(' ', '-');
+            final var title = album.getName().toLowerCase().replace(' ', '-');
+            final var crawled = crawler.getAlbumGenresRating(title, artist);
+            return new MusicDto.NowPlaying.Response(new MusicDto.AlbumFullInfo.Info(album, crawled));
         } catch (IOException | SpotifyWebApiException | ParseException e) {
             throw new SpotifyNowPlayingException(e.getMessage());
         }
