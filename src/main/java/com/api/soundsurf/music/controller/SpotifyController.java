12c1698979f68dd9dcec0b761b9c645375c411e0
roganOhDev <fdscbjdcnhd@gmail.com>
2024-05-12 08:50:40 +0900

    delete: delete debug code

--- src/main/java/com/api/soundsurf/music/controller/SpotifyController.java
+++ src/main/java/com/api/soundsurf/music/controller/SpotifyController.java
@@ -15,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -38,11 +39,6 @@ public class SpotifyController {
         return driveService.recommendation(request);
     }
 
-    @GetMapping("/")
-    public String[] a() {
-        return crawlerService.getMusicGenresRating("butter", "bts");
-    }
-    
     @GetMapping("/search")
     @Operation(
             parameters = {
