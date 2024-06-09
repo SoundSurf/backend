29dae6d186d59a18b7609710c4879a5cfacfec99
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-06-09 20:33:21 +0900

    feat: QR S3 upload

--- src/main/java/com/api/soundsurf/iam/QrProcessor.java
+++ src/main/java/com/api/soundsurf/iam/QrProcessor.java
@@ -1,18 +1,23 @@
 package com.api.soundsurf.iam;
 
+import com.api.soundsurf.api.utils.S3ImageUploader;
 import com.api.soundsurf.iam.exception.CannotCreateQRException;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class QrProcessor {
+
+    private final S3ImageUploader s3ImageUploader;
 
     // TODO: QR 코드 URL 정하기
     private final String baseUrl = "http://" + System.getenv("DB_HOST") + ":" + System.getenv("DB_PORT");
@@ -21,9 +26,8 @@ public class QrProcessor {
         final var url = baseUrl + "/api/v1/qr/" + userEmail;
 
         try {
-            generateQrCodeImage(url);
-            return "";
-            //TODO : save in s3
+            byte[] qrCodeImage = generateQrCodeImage(url);
+            return uploadQrCodeToS3(qrCodeImage, userEmail);
         } catch (WriterException | IOException e) {
             throw new CannotCreateQRException(e.getMessage());
         }
@@ -36,5 +40,10 @@ public class QrProcessor {
         MatrixToImageWriter.writeToStream(matrix, "PNG", outputStream);
 
         return outputStream.toByteArray();
+    }
+
+    private String uploadQrCodeToS3(byte[] qrCodeImage, String userEmail) throws IOException {
+        String fileName = userEmail + ".png";
+        return s3ImageUploader.uploadImage(qrCodeImage, fileName, "qr");
     }
 }
