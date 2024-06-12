2cb83fea11d5e36960f9a910aa93ad92a206fe78
yuujinkim <yuuuuu01@naver.com>
2024-06-12 17:08:08 +0900

    feat: saved_music controller

--- src/main/java/com/api/soundsurf/iam/controller/ProfileController.java
+++ src/main/java/com/api/soundsurf/iam/controller/ProfileController.java
@@ -3,6 +3,7 @@ package com.api.soundsurf.iam.controller;
 import com.api.soundsurf.iam.domain.user.UserTransferService;
 import com.api.soundsurf.iam.dto.SessionUser;
 import com.api.soundsurf.iam.dto.UserProfileDto;
+import com.api.soundsurf.music.domain.SavedMusicService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -22,6 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 @CrossOrigin("*")
 public class ProfileController {
     private final UserTransferService userTransferService;
+    private final SavedMusicService savedMusicService;
 
     @PatchMapping(value = "")
     @Operation(
@@ -44,5 +46,23 @@ public class ProfileController {
     public UserProfileDto.Qr.Response getQr(final @AuthenticationPrincipal SessionUser sessionUser) {
         return userTransferService.getQr(sessionUser);
     }
+    @PostMapping(value = "/music/save")
+    @Operation(
+            parameters = {
+                    @Parameter(name = "authorization", in = ParameterIn.HEADER,
+                            required = true, content = @Content(mediaType = "application/json"))
+            })
+    public void save(final @AuthenticationPrincipal SessionUser sessionUser, final @RequestParam Long musicId) {
+        savedMusicService.saveMusic(sessionUser.getUserId(), musicId);
+    }
 
+    @DeleteMapping(value = "/music/unsave")
+    @Operation(
+            parameters = {
+                    @Parameter(name = "authorization", in = ParameterIn.HEADER,
+                            required = true, content = @Content(mediaType = "application/json"))
+            })
+    public void unsave(final @AuthenticationPrincipal SessionUser sessionUser, final @RequestParam Long musicId) {
+        savedMusicService.unsaveMusic(sessionUser.getUserId(), musicId);
+    }
 }
